59b1c709bb9b9575cd7ec9fa7b8d13c7cc8c80c7
parsh24 <parsh.cosmos11@gmail.com>
2018-10-02 18:06:38 +0530

    Added: ContractExecDetails fxn

--- src/utils/ethereum.js
+++ src/utils/ethereum.js
@@ -1,6 +1,6 @@
 const web3 = require('./web3');
 
-const etherTransferDetails = txnDetails => {
+const accountTransferDetails = txnDetails => {
   const details = {
     block: {
       blockHeight: txnDetails.blockNumber
@@ -28,8 +28,6 @@ const etherTransferDetails = txnDetails => {
 };
 
 const tokenTransferDetails = txnDetails => {
-  //const methodID = txnDetails.input.slice(0, 10);
-
   const recipient = txnDetails.input.slice(10, 10 + 64).replace(/^0+/, '');
   const value = parseInt('0x' + txnDetails.input.slice(74, 74 + 64));
 
@@ -67,6 +65,41 @@ const tokenTransferDetails = txnDetails => {
   return details;
 };
 
+const contractExecutionDetails = txnDetails => {
+  const details = {
+    block: {
+      blockHeight: txnDetails.blockNumber
+    },
+    outs: [
+      {
+        address: txnDetails.to,
+        value: +txnDetails.value,
+        type: 'transfer',
+        coinspecific: {
+          tracehash: txnDetails.hash
+        }
+      }
+    ],
+    ins: [
+      {
+        address: txnDetails.from,
+        value: -txnDetails.value,
+        type: 'transfer',
+        coinspecific: {
+          tracehash: txnDetails.hash
+        }
+      }
+    ],
+    hash: txnDetails.hash,
+    currency: 'ETH',
+    state: txnDetails.blockNumber ? 'confirmed' : 'pending',
+    depositType: 'Contract',
+    chain: 'ETH.main'
+  };
+
+  return details;
+};
+
 const transferDetails = async transactionHash => {
   const txnDetails = await web3.eth.getTransaction(transactionHash);
 
@@ -76,11 +109,19 @@ const transferDetails = async transactionHash => {
     );
   }
 
-  const tokenTransferValue = txnDetails.input.slice(74, 74 + 64);
-  if (tokenTransferValue) {
-    return tokenTransferDetails(txnDetails);
+  const methodID = txnDetails.input.slice(2, 10);
+
+  if (!methodID) {
+    // it's an account transfer transaction
+    return accountTransferDetails(txnDetails);
   } else {
-    return etherTransferDetails(txnDetails);
+    if (methodID == 'a9059cbb') {
+      // it's a token transfer transaction
+      return tokenTransferDetails(txnDetails);
+    } else {
+      // it's a contract execution transaction
+      return contractExecutionDetails(txnDetails);
+    }
   }
 };
 
