8503b9f990726dcd6cc2a45f94dcd431f15fc37d
parsh24 <parsh.cosmos11@gmail.com>
2018-10-02 18:07:20 +0530

    Unit test: Contract Execution txn

--- test/ethereum.test.js
+++ test/ethereum.test.js
@@ -4,37 +4,47 @@ const { transferDetails } = require('../src/utils/ethereum');
 
 describe('Ethereum Utility Tests', () => {
   before(() => {
-    dummyAccountTxnHash =
-      '0xf18741369405855431aed68d9faec18d5dc94658f5657ed44dabb27466af0685';
+    dummyAccountTransferTxnID =
+      '0x29b40daa7403dad36d20425f9d397657e54e3ec5d0a2cfef2454d4e100e17775';
 
-    dummyTokenTxnHash =
+    dummyTokenTxnID =
       '0xeb297afc3377169816f95c8e5ec15b7f5a231be769195305acd8a231b5166eb4';
 
-    invalidTxnHash =
-      '0xfjaljfdk7169816f95c8e5ec153l42q3h44j32l4hjdkafkjahfdaeb4';
+    dummyContractExecutionTxnID =
+      '0x49dc5e129fa4cfa50e87c0db44ed47b320f17234271c513b8abe5b2a8cd1fac3';
 
-    rinkebyTxnHash =
+    invalidTxnID = '0xfjaljfdk7169816f95c8e5ec153l42q3h44j32l4hjdkafkjahfdaeb4';
+
+    rinkebyTxnID =
       '0x81e2e5fad46e7f622a8b760f2c58b1992f3ecd3b68bcb7da9572184574cd3736';
   });
 
-  it('should supply the details corresponding to the given account transfer transaction', async () => {
-    const details = await transferDetails(dummyAccountTxnHash);
+  it('should provide the details corresponding to the supplied account transfer transaction', async () => {
+    const details = await transferDetails(dummyAccountTransferTxnID);
     expect(details).toBeDefined();
-    expect(details.hash).toEqual(dummyAccountTxnHash);
+    expect(details.hash).toEqual(dummyAccountTransferTxnID);
     expect(details.depositType).toEqual('account');
   });
 
-  it('should supply the details corresponding to the given contract(ERC20 token) transaction', async () => {
-    const details = await transferDetails(dummyTokenTxnHash);
+  it('should provide the details corresponding to the supplied ERC20 Token transfer transaction', async () => {
+    const details = await transferDetails(dummyTokenTxnID);
     expect(details).toBeDefined();
-    expect(details.hash).toEqual(dummyTokenTxnHash);
-    expect(details.ins[0].coinspecific.tokenAddress).toBeDefined();
+    expect(details.hash).toEqual(dummyTokenTxnID);
     expect(details.depositType).toEqual('Contract');
+    expect(details.ins[0].coinspecific.tokenAddress).toBeDefined();
+  });
+
+  it('should provide the details corresponding to the supplied contract execution transaction', async () => {
+    const details = await transferDetails(dummyContractExecutionTxnID);
+    expect(details).toBeDefined();
+    expect(details.hash).toEqual(dummyContractExecutionTxnID);
+    expect(details.depositType).toEqual('Contract');
+    expect(details.ins[0].coinspecific.tracehash).toBeDefined();
   });
 
   it('should throw an error message against an invalid transactionID', async () => {
     try {
-      await transferDetails(invalidTxnHash);
+      await transferDetails(invalidTxnID);
     } catch (err) {
       expect(err.message).toBeTruthy();
       //console.log(err.message);
@@ -43,7 +53,7 @@ describe('Ethereum Utility Tests', () => {
 
   it('should throw an error message against test network transactionID', async () => {
     try {
-      await transferDetails(rinkebyTxnHash);
+      await transferDetails(rinkebyTxnID);
     } catch (err) {
       expect(err.message).toBeTruthy();
       //console.log(err.message);
