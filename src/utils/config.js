424eae1beea87b54fa812f9fb3229bce428fa1e5
parsh24 <parsh.cosmos11@gmail.com>
2018-10-02 13:42:55 +0530

    switched n/w to mainnet

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,6 @@
 const PORT = process.env.PORT || 4040;
 const NETWORK_URL =
-  'https://rinkeby.infura.io/v3/b4e2b3bf723f4985ae36bc838089e50d';
-
+  'https://mainnet.infura.io/v3/6dfe769f94364017b82a58d6b5c3543e';
 module.exports = {
   PORT,
   NETWORK_URL
