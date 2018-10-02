915d8caa5d6f4f876a3e1473e3a5704ee3a76451
parsh24 <parsh.cosmos11@gmail.com>
2018-10-02 15:58:06 +0530

    test based modifications

--- src/utils/ethereum.js
+++ src/utils/ethereum.js
@@ -84,14 +84,6 @@ const transferDetails = async transactionHash => {
   }
 };
 
-// const dummyAccountTxnHash =
-//   '0xf18741369405855431aed68d9faec18d5dc94658f5657ed44dabb27466af0685';
-// transferDetails(dummyAccountTxnHash);
-
-// const dummyTokenTxnHash =
-//   '0xeb297afc3377169816f95c8e5ec15b7f5a231be769195305acd8a231b5166eb4';
-// transferDetails(dummyTokenTxnHash);
-
 module.exports = {
   transferDetails
 };
